810cb7879812547ff622bf6f2b9f7e685a9a9e48
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2025-11-15 06:44:31 -0500

    Added code demonstrating the usage of the AND specification

--- v1_hard_coded_spec/src/main/java/v3_composite_spec/Main.java
+++ v1_hard_coded_spec/src/main/java/v3_composite_spec/Main.java
@@ -3,6 +3,7 @@ package v3_composite_spec;
 import java.util.Set;
 
 import v3_composite_spec.Baseball.BaseballSeat;
+import v3_composite_spec.spec_library.AndSpecification;
 import v3_composite_spec.spec_library.LeafSpecification;
 import v3_composite_spec.spec_library.Specification;
 
@@ -60,6 +61,43 @@ public class Main {
                 System.out.println(seat);
             }
         });
+
+        System.out.println("\n\n");
+
+
+        System.out.println("A fourth customer wants a cheap AND accessible seat");
+        System.out.println("This time, we create a CompositeSpecification and add our existing cheap seat spec.");
+
+        Specification<BaseballSeat> cheap_and_accessible_spec = new AndSpecification<>(cheap_seat_spec,accessible_seat_spec);
+
+        baseballSeats.stream().forEach(seat -> {
+            if(cheap_and_accessible_spec.isSatisfiedBy(seat)) {   
+                System.out.println(seat);
+            }
+        });
+
+        System.out.println("\n\n");
+
+
+        System.out.println("A fifth customer wants a cheap AND accessible seat.");
+        System.out.println("This customer has a different notion of cheap.");
+        System.out.println("This time, we create a new LeafSpecification and create a CompositeSpecification and add our existing accessible seat spec.");
+
+        Specification<BaseballSeat> cheap_seat_spec_v2 = 
+            new LeafSpecification<BaseballSeat>(
+                seat -> seat.getCost() <= 115.00
+            );
+
+        Specification<BaseballSeat> cheap_and_accessible_spec_v2 = new AndSpecification<>(cheap_seat_spec_v2,accessible_seat_spec);
+
+        baseballSeats.stream().forEach(seat -> {
+            if(cheap_and_accessible_spec_v2.isSatisfiedBy(seat)) {   
+                System.out.println(seat);
+            }
+        });
+
+        System.out.println("\n\n");
+
 
     }
 }
